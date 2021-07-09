bbc9b5bcd9d9d5c31a275f9eeaba33b1d8c8765f
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-07-09 12:18:56 +0900

    2주차 1일 - HiHiRobot 추가

---
+++ Assets/Wall.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public float speed = -5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (transform.position.x < -10)
+            Destroy(gameObject);
+    }
+}
